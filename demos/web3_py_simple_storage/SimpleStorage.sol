1d445c7807029c37ba44eaf864fa2cf92fce60df
Julian Goh <juliangoh5@gmail.com>
2022-04-26 15:58:13 +0800

    26/4 - Finished up Remix challenges, started Web3.py intro
    - Sooooo much time wasted on bugs from the course material

---
+++ demos/web3_py_simple_storage/SimpleStorage.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage{
+
+    // Not necessary to initialise explicitly, defaults to 0
+    uint256 favouriteNumber;
+
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+    
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() public view returns(uint256){
+        return favouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public{
+        people.push(People(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
+
