079a22529e6995b578612cd9e9cadfdb4c032c08
Julian Goh <juliangoh5@gmail.com>
2022-04-26 17:32:48 +0800

    26/4/22 - Setup Ganache node

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -1,5 +1,6 @@
 from solcx import compile_standard, install_solc
 import json
+from web3 import Web3
 
 install_solc("0.6.0")
 
@@ -32,4 +33,12 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 # get abi
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 5777
+my_address = "0xF2d06077239C225e4921C79BF9b4dA27485Da2D6"
+private_key = "0x5fc576e3a3db725cd560f1e257978eb9e16cfb802c8f818cf5b1f7eb949ed9b0"
 
+# Create contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print(SimpleStorage)
