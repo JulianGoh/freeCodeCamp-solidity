806dbef22895a1c3852d767dc75ba01e298f9f0c
Julian Goh <juliangoh5@gmail.com>
2022-05-02 16:55:09 +0800

    2/5/22 - Build / send / sign txn (in progress)
    Found more bugs with the tutorial 🤮

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -1,12 +1,16 @@
 from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 install_solc("0.6.0")
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+    # print(simple_storage_file)
 
 # Compile Solidity
 compiled_sol = compile_standard(
@@ -35,10 +39,31 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # for connecting to ganache
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-chain_id = 5777
-my_address = "0xF2d06077239C225e4921C79BF9b4dA27485Da2D6"
-private_key = "0x5fc576e3a3db725cd560f1e257978eb9e16cfb802c8f818cf5b1f7eb949ed9b0"
+chain_id = 1337
+my_address = "0xBC3d63756F43D0C79545E035Bd3A0ea538C1852D"
+private_key = os.getenv("PRIVATE_KEY")
+print(private_key)
 
 # Create contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-print(SimpleStorage)
+
+# Get latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
+
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+        "maxFeePerGas": w3.toWei("2", "gwei"),
+        "maxPriorityFeePerGas": w3.toWei("1", "gwei"),
+    }
+)
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
